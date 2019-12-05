a9bdb5badf2962788dbf5ed3b2def2d70c31af58
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-05 18:51:02 -0500

    connect to DB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 // routes
 app.get('/', (req, res) => {
@@ -10,6 +11,13 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
     res.send('We are on posts');
 });
+
+// connect to db
+mongoose.connect(
+    process.env.DB_CONNECTION,
+     { useNewUrUrlParser: true },  () => 
+    console.log('Connected to DB!')
+);
 // start listening to server by..
 
 app.listen(3000);
